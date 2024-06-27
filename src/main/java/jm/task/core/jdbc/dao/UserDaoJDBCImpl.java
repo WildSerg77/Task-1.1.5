b663de311e3e0b403ecd553b74cde3a212341b5e
Sergey Kalenkov <wildserg@mail.ru>
2024-06-27 17:36:54 +0300

    AutoCommit Off

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,60 +2,78 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
     Connection conn = Util.getConnection();
-    String tableCreate = "CREATE TABLE users (Id BIGINT PRIMARY KEY AUTO_INCREMENT, Name VARCHAR(20), LastName VARCHAR (20), Age TINYINT)";
-
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-        try {
-            Statement statement = conn.createStatement();
+        String tableCreate = "CREATE TABLE IF NOT EXISTS users (Id BIGINT PRIMARY KEY AUTO_INCREMENT, Name VARCHAR(20), LastName VARCHAR (20), Age TINYINT)";
+        try (Statement statement = conn.createStatement()) {
             statement.executeUpdate(tableCreate);
+            conn.commit();
         } catch (SQLException e) {
+            System.out.println("SQLException. Executing rollback");
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+            }
         }
     }
 
     public void dropUsersTable() {
-        try {
-            Statement statement = conn.createStatement();
-            statement.executeUpdate("DROP TABLE users");
+        try (Statement statement = conn.createStatement()) {
+            statement.executeUpdate("DROP TABLE IF EXISTS users");
+            conn.commit();
         } catch (SQLException e) {
+            System.out.println("SQLException. Executing rollback");
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+            }
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
         try {
-            Statement statement = conn.createStatement();
-            statement.executeUpdate("INSERT INTO users (Name, LastName, Age) VALUES ('" + name + "', '" + lastName + "', " + age + ")");
+            PreparedStatement statement = conn.prepareStatement("INSERT INTO users (Name, LastName, Age) VALUES (?, ?, ?)");
+            statement.setString(1, name);
+            statement.setString(2, lastName);
+            statement.setByte(3, age);
+            statement.executeUpdate();
+            conn.commit();
             System.out.printf("User с именем — %s добавлен в базу данных\n", name);
+            statement.close();
         } catch (SQLException e) {
+            System.out.println("SQLException. Executing rollback");
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+            }
         }
     }
 
     public void removeUserById(long id) {
-        try {
-            Statement statement = conn.createStatement();
+        try (Statement statement = conn.createStatement()) {
             statement.executeUpdate("DELETE FROM users WHERE Id = " + id);
+            conn.commit();
         } catch (SQLException e) {
+            System.out.println("SQLException. Executing rollback");
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+            }
         }
-
     }
 
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
-        try {
-            Statement statement = conn.createStatement();
+        try (Statement statement = conn.createStatement()) {
             ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
             while (resultSet.next()) {
                 users.add(new User(resultSet.getString("Name"), resultSet.getString("LastName"), resultSet.getByte("Age")));
@@ -63,16 +81,22 @@ public class UserDaoJDBCImpl implements UserDao {
             for (User user : users) {
                 System.out.println(user.toString());
             }
+            resultSet.close();
         } catch (SQLException e) {
         }
         return users;
     }
 
     public void cleanUsersTable() {
-        try {
-            Statement statement = conn.createStatement();
-            statement.executeUpdate("DELETE FROM users");
+        try (Statement statement = conn.createStatement()) {
+            statement.executeUpdate("TRUNCATE users");
+            conn.commit();
         } catch (SQLException e) {
+            System.out.println("SQLException. Executing rollback");
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+            }
         }
 
     }
